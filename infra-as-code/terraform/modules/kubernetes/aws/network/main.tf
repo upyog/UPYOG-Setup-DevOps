237fa6cd232fbfb43cb654966369efb7095f0596
jnan1234 <41353401+jnan1234@users.noreply.github.com>
2024-07-19 13:10:33 +0530

    Update main.tf

--- infra-as-code/terraform/modules/kubernetes/aws/network/main.tf
+++ infra-as-code/terraform/modules/kubernetes/aws/network/main.tf
@@ -105,8 +105,8 @@ resource "aws_eip" "eip" {
 }
 
 resource "aws_nat_gateway" "nat" {
-  #allocation_id = "${aws_eip.eip.id}"
-  allocation_id = "eipalloc-04169e7acd1f66ccd"
+  allocation_id = "${aws_eip.eip.id}"
+  #allocation_id = "eipalloc-04169e7acd1f66ccd"
   subnet_id     = "${element(aws_subnet.public_subnet.*.id, 0)}"
 
   depends_on = [aws_internet_gateway.internet_gateway]
